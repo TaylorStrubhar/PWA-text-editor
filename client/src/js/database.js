eff4c80be0755459a9fb37eb17b844125c5fd6b3
Ira Strubhar <taylor.strubhar@gmail.com>
2022-09-09 21:47:49 -0700

    src js updated

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction('jate', 'readwrite');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.put({ id: 1, value: content });
+  const result = await request;
+  
+  console.log('Data was saved to the database', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const txtEditorDB = await openDB('jate', 'readonly');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.get(1);
+  const result = await request;
+  result
+    ? console.log('Data was retrieved from the database')
+    : console.log('Data was not found in the database');
+};
 
 initdb();
